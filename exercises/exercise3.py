792b8754da07b301360b058b924740dd962ebc04
abranico <abranico011@gmail.com>
2023-09-25 14:20:36 -0300

    ejercicio3

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -14,6 +14,50 @@ class Article:
         - No utilizar Dataclasses
         - Utilizar Type Hints en todos los métodos y variables
     """
+    IVA = 0.21
+
+    @classmethod
+    def actualizar_iva(cls, nuevo_iva):
+        cls.IVA = nuevo_iva
+
+    def __init__(self, nombre, costo, descuento=0):
+        self._nombre = nombre
+        self._costo = costo
+        self._descuento = descuento
+        self._precio = self.calcular_precio()
+
+    def calcular_precio(self) -> float:
+        resultado = (self._costo * (self.IVA+1))
+        resultado -= (resultado * self._descuento)
+        return round(resultado, 2)
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nuevo_nombre):
+        self._nombre = nuevo_nombre
+
+    @property
+    def costo(self):
+        return self._costo
+
+    @costo.setter
+    def costo(self, nuevo_costo):
+        self._costo = nuevo_costo
+
+    @property
+    def descuento(self):
+        return self._descuento
+
+    @descuento.setter
+    def descuento(self, nuevo_descuento=0):
+        self._descuento = nuevo_descuento
+
+    @property
+    def precio(self):
+        return self._precio
 
 
 # NO MODIFICAR - INICIO
